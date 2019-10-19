78d6ac0f140a6c529c9088273bfa5edb4febee54
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2019-10-19 23:01:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyASXpq7GdCIgveKL9fSCQY6e1wOfA8X7Io",
-    authDomain: "fitsixes2k19.firebaseapp.com",
-    databaseURL: "https://fitsixes2k19.firebaseio.com",
-    projectId: "fitsixes2k19",
-    storageBucket: "fitsixes2k19.appspot.com",
-    messagingSenderId: "790480965220",
-    appId: "1:790480965220:web:f35c651857c30dfed0d971",
-    measurementId: "G-W7654W8HPP"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
